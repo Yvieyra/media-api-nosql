7a2d5e6fd8c540db93b21d693131142226a6073a
Ying Yang <vieyrayessenia8@gmail.com>
2023-03-09 16:31:19 -0700

    added User model details

--- models/User.js
+++ models/User.js
@@ -9,14 +9,41 @@ const userSchema = new Schema(
         userName: {
             type: String,
             unique: true,
-            required: true, 
-            trim: true, 
+            required: true,
+            trim: true,
         },
         email: {
-            type: String, 
+            type: String,
             required: true,
-            unique: true, 
-            
-        } 
-    }
-)
+            unique: true,
+            match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+        toJSON: {
+            virtuals: true,
+        },
+        id: false, //what is this id:false?
+    },
+);
+
+userSchema
+    .virtual('friendCount')
+    //getter
+    .get(function () {
+        return this.friends.length;
+    });
+
+const User = model('User', userSchema);
+
+module.exports = User;
